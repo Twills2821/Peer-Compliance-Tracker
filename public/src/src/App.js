e88b69b3731de1e402180f8f1253f909309ebcd3
Twills2821 <thaddeuslwilliams@gmail.com>
2025-08-24 08:16:11 -0400

    Create App.js

---
+++ public/src/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import PeerBillingTracker from './components/PeerBillingTracker';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <PeerBillingTracker />
+    </div>
+  );
+}
+
+export default App;
